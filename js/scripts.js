554beb8cc0896ac2a4af3fcf2d4729bae0c3ee68
Jaki Widby <jaki.widby@gmail.com>
2020-08-13 13:34:07 -0700

    add variables to hold input values, add submit function

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,32 @@ function determineTriangleType(side1, side2, side3) {
   }
   return triangle;
 }
+
+
+$(document).ready(function() {
+  $("form#triangle").submit(function() {
+    event.preventDefault();
+    const side1 = parseInt($("input#side1").val());
+    const side2 = parseInt($("input#side2").val());
+    const side3 = parseInt($("input#side3").val());
+
+    determineTriangleType(side1, side2, side3);
+
+    if (triangle === "equilateral") {
+      $("div").hide();
+      $("div").show();
+      $("#equilateral").show();
+    } else if (triangle === "isosceles") {
+      $("div").hide();
+      $("div").show();
+      $("#isosceles").show();
+    } else if (triangle === "scalene") {
+      $("div").hide();
+      $("div").show();
+      $("#scalene").show();
+    } else {
+      alert("Those measurements won't create a triangle.");
+    }
+
+  });
+});
