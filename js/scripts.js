8d03aba7ad6fc40caa753e2d8058264e8d608a44
Jaki Widby <jaki.widby@gmail.com>
2020-08-13 14:08:28 -0700

    define triangle in submit function

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 function determineTriangleType(side1, side2, side3) {
   let triangle;
 
-  if (side1 === side2 && side1=== side3) {
-    triangle = "equialateral";
+  if (side1 === side2 && side1 === side3) {
+    triangle = "equilateral";
   } else if (side1 === side2 || side1 === side3 || side2 === side3) {
     triangle = "isosceles";
   } else if (side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1) {
@@ -10,6 +10,8 @@ function determineTriangleType(side1, side2, side3) {
   } else {
     triangle = "scalene";
   }
+  
+  console.log(triangle);
   return triangle;
 }
 
@@ -20,8 +22,8 @@ $(document).ready(function() {
     const side1 = parseInt($("input#side1").val());
     const side2 = parseInt($("input#side2").val());
     const side3 = parseInt($("input#side3").val());
-
-    determineTriangleType(side1, side2, side3);
+    console.log(side1, side2, side3);
+    let triangle = determineTriangleType(side1, side2, side3);
 
     if (triangle === "equilateral") {
       $("#result").hide();
